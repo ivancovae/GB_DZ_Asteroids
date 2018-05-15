1f5f4fdaecbaea8b3402fbf0a8bfc5a5333abd2a
ivancovae <ivancovae@gmail.com>
2018-05-15 12:17:32 +0300

    Третье задание с комментированием второго, fix minor

--- HW_Asteroids/Program.cs
+++ HW_Asteroids/Program.cs
@@ -7,14 +7,60 @@ namespace HW_Asteroids
 {
     static class Program
     {
+        static int OrderBy_Method(KeyValuePair<string, int> pair)
+        {
+            return pair.Value;
+        }
         static void Main(string[] args)
         {
+            // Проверка для задания 2 - 
+            //            Дана коллекция List<T>, требуется подсчитать, сколько раз каждый элемент встречается в
+            //                  данной коллекции.
+            //              а) для целых чисел;
+            //              б) *для обобщенной коллекции;
+            //              в)**используя Linq.
             List<char> list = new List<char> { '1', '2', '4', '2', '5', '6', '1', '4', '2', '6', '3', '5', '7', '8', '3', '7', '3', '7', '4', '1', '7', '2', '5', '3' };
+            // количество для элемента
             var countUniqueElem = list.GetCountRepeat('4');
+            // количество для индекса
             var countUniqueIndex = list.GetCountRepeat(5);
+            // все уникальные
             var UniqueAll = list.GetUniques();
+            // количество повторений для всех элементов через Linq
             var countUniqueAll = list.GetAllCountRepeatLinq();
-            Console.WriteLine(1);
+
+            // Задание 3 - Дан фрагмент программы:
+            //            а) Свернуть обращение к OrderBy с использованием лямбда - выражения$
+            //            б) *Развернуть обращение к OrderBy с использованием делегата Predicate<T>. Исправления условия преподователем - "заменить Predicate<T> на Func<KeyValuePair<string, int>, int>"
+
+            Dictionary<string, int> dict = new Dictionary<string, int>
+            {
+                { "four", 4 },
+                { "two", 2 },
+                { "one", 1 },
+                { "three", 3 },
+            };
+
+            // Вариант из методички 
+            #region old
+            var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; });
+            #endregion
+
+            // через лямбда-выражения
+            #region task_3_A
+            var d_3_A = dict.OrderBy(pair => pair.Value);
+            #endregion
+            
+            // развернутый вариант через Func<KeyValuePair<string, int>, int>
+            #region task_3_B
+            Func<KeyValuePair<string, int>, int> d1 = new Func<KeyValuePair<string, int>, int>(OrderBy_Method);
+            var d_3_B = dict.OrderBy(d1);
+            #endregion
+
+            foreach (var pair in d_3_B)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
 
             Log.OnChangeMessage += ChangeMessage;
             FileLog.Start();
