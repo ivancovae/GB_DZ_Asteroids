2a28d329a032a401c6396530574967c7ba322579
ivancovae <ivancovae@gmail.com>
2018-05-15 10:11:13 +0300

    Второе задание сделал обобщение, включает в себя и целые числа + доп. метод с помощью Linq

---
+++ HW_Asteroids/ListExtension.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW_Asteroids
+{
+    public static class MyExtention
+    {
+        /// <summary>
+        /// Получить количество повторения для элемента в обобщеном списке
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Исключение, которое выдается, если значение аргумента не соответствует допустимому диапазону значений, установленному вызванным методом</exception>
+        /// <typeparam name="T">обобщенный тип</typeparam>
+        /// <param name="list">метод расширяет класс List<T></param>
+        /// <param name="forElement">элемент, для которого ищутся повторения</param>
+        /// <returns>количество повторений</returns>
+        public static int GetCountRepeat<T>(this List<T> list, T forElement) where T:IEquatable<T>
+        {
+            if(list.IndexOf(forElement) == -1)
+                throw new ArgumentOutOfRangeException($"forElement", $"Элемент не найден в коллекции");
+
+            int count = 0;
+            foreach (T val in list)
+            {
+                if ((val as IEquatable<T>).Equals(forElement))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        /// <summary>
+        /// Получить количество повторения для элемента в обобщеном списке по индексу
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Исключение, которое создается при передаче пустой ссылки методу, который не принимает ее как допустимый аргумент</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Исключение, которое выдается, если значение аргумента не соответствует допустимому диапазону значений, установленному вызванным методом</exception>
+        /// <typeparam name="T">обобщенный тип</typeparam>
+        /// <param name="list">метод расширяет класс List<T></param>
+        /// <param name="forIndex">индекс элемента, для которого ищутся повторения</param>
+        /// <returns>количество повторений</returns>
+        public static int GetCountRepeat<T>(this List<T> list, int forIndex) where T : IEquatable<T>
+        {
+            return list.GetCountRepeat<T>(list.ElementAt(forIndex));
+        }
+        /// <summary>
+        /// Получить список уникальных элементов
+        /// </summary>
+        /// <typeparam name="T">обобщенный тип</typeparam>
+        /// <param name="list">метод расширяет класс List<T></param>
+        /// <returns>коллекцию уникальных элементов списка</returns>
+        public static ICollection<T> GetUniques<T>(this List<T> list)
+        {
+            var found = new Dictionary<T, bool> { };
+            List<T> uniques = new List<T> { };
+            foreach (T val in list)
+            {
+                if (!found.ContainsKey(val))
+                {
+                    found[val] = true;
+                    uniques.Add(val);
+                }
+            }
+            return uniques;
+        }
+        /// <summary>
+        /// Получить словарь количества повторений элементов в обобщенном списке
+        /// </summary>
+        /// <typeparam name="T">обобщенный тип</typeparam>
+        /// <param name="list">метод расширяет класс List<T></param>
+        /// <returns>словарь с парой ключ\значение, где ключ ссылка на объект, значение количество повторений в списке</returns>
+        public static Dictionary<T, int> GetAllCountRepeat<T>(this List<T> list) where T : IEquatable<T>
+        {
+            var listUniques = list.GetUniques();
+            var result = new Dictionary<T, int> { };
+            foreach(T val in listUniques)
+            {
+                result[val] = list.GetCountRepeat<T>(val);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Получить словарь количества повторений элементов в обобщенном списке с помощью Linq
+        /// </summary>
+        /// <typeparam name="T">обобщенный тип</typeparam>
+        /// <param name="list">метод расширяет класс List<T></param>
+        /// <returns>словарь с парой ключ\значение, где ключ ссылка на объект, значение количество повторений в списке</returns>
+        public static Dictionary<T, int> GetAllCountRepeatLinq<T>(this List<T> list) where T : IEquatable<T>
+        {
+            return list.GroupBy(e => e)
+                       .Select(g => new { Element = g.Key, Counter = g.Count() })
+                       .ToDictionary(g => g.Element, g => g.Counter);
+        }
+
+
+    }
+}
--- HW_Asteroids/Program.cs
+++ HW_Asteroids/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace HW_Asteroids
@@ -7,6 +9,13 @@ namespace HW_Asteroids
     {
         static void Main(string[] args)
         {
+            List<char> list = new List<char> { '1', '2', '4', '2', '5', '6', '1', '4', '2', '6', '3', '5', '7', '8', '3', '7', '3', '7', '4', '1', '7', '2', '5', '3' };
+            var countUniqueElem = list.GetCountRepeat('4');
+            var countUniqueIndex = list.GetCountRepeat(5);
+            var UniqueAll = list.GetUniques();
+            var countUniqueAll = list.GetAllCountRepeatLinq();
+            Console.WriteLine(1);
+
             Log.OnChangeMessage += ChangeMessage;
             FileLog.Start();
             Form form = new Form
